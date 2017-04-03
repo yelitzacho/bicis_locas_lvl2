911a0d58db90d66962fba140f6705e5482864f98
Yelitza Cho <yelitza.choque@gmail.com>
2017-04-03 17:50:03 -0500

    Cambio

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
 function validateForm(){
-	/* Escribe tú código aquí */
+    var nombre = document.getElementById('name').value;
+    var apellido = document.getElementById('lastname').value;
+    var email = document.getElementById('input-email').value;
+    var password = document.getElementById('input-password').value;
+    var tipoDeBici = document.getElementsByClassName("form-control").value;
+    var twitter = document.getElementById('input-social');
+
+    if(nombre === "" || apellido === "" || email === "" || password === "" || twitter === ""){
+      alert('[ERROR] Todos los campos son obligatorios, excepto el de las bicis');
+    }
+    else if (!(/^([A-Z]{1}[a-z]+[\s]*)+$/.test(nombre))) {
+      alert('[ERROR] El campo Nombre debe tener la primera letra en mayúsculas');
+    }
+    else if (!(/^([A-Z]{1}[a-z]+[\s]*)+$/.test(apellido))) {
+      alert('[ERROR] El campo Apellido debe tener la primera letra en mayúsculas');
+    }
+    else if (!(/[\w]+@{1}[\w]+\.[a-z]{2,3}/.test(correo))) {
+      alert('[ERROR] El campo email tiene que tener el siguiente formato: name@domain.com');
+    }
+    else if (password.length<=6 || password === "password" || password === "123456" || password === "098765") {
+      alert('[ERROR] El campo contraseña tiene que tener mínimo 6 caracteres.');
+    }
+    else {
+      alert('[ERROR] Debe rellenar todos los campos');
+    }
 }
